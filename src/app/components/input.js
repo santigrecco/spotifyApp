ab997195a659c41fc37b386c7fedce9d2f4bcfe9
Santiago Grecco <santiago.grecco@globant.com>
2016-11-24 16:31:33 -0300

    input error bug fixed

--- src/app/components/input.js
+++ src/app/components/input.js
@@ -3,18 +3,24 @@ export var inputComponent =  {
     controllerAs: 'myCtrl',
     templateUrl: '/app/components/input.html',
     bindings: {
-      refresh: '='
+      // refresh: '='
     }
   }
 
-  function inputController($scope, $rootScope, $location,  ApiService){
+  function inputController($scope, $rootScope, $location, $timeout, ApiService){
     $scope.ic = this;
     this.search = '';
     this.error = false;
+
     $scope.go = function(search){
       if(search == ''){
         this.error = true;
+        $timeout(()=>{
+          this.error = false;
+        }, 2000);
+
       }else{
+        this.error = false;
         ApiService.search = search;
         $location.path(`/results/${search}`);
       }
